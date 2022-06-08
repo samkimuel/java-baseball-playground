724a32c830f922d1583da620ad8b00fb0a390036
samkimuel <samkim2626@gmail.com>
2022-06-08 22:53:21 +0900

    test: String 클래스에 대한 학습 테스트-요구사항 1
    - "1,2"을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트를 구현한다.
    - "1"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트를 구현한다.

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,22 @@
 package study;
 
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 public class StringTest {
-    @Test
-    void replace() {
-        String actual = "abc".replace("b", "d");
-        assertThat(actual).isEqualTo("adc");
-    }
+
+	@Test
+	void replace() {
+		String actual = "abc".replace("b", "d");
+		assertThat(actual).isEqualTo("adc");
+	}
+
+	@Test
+	void split() {
+		String[] actual = "1,2".split(",");
+
+		assertThat(actual).contains("1");
+		assertThat(actual).containsExactly("1", "2");
+	}
 }
